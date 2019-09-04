3b151fcd298f189f2512ffea5e2a6db1bf257cc0
MarcusCaldeira <mac.caldr@gmail.com>
2019-09-04 17:22:05 -0500

    this is loaded with a test, im gonna see how it works. I will def need tutoring this saturday.

--- CaldeiraProgram1/src/Invoice.java
+++ CaldeiraProgram1/src/Invoice.java
@@ -1,14 +1,32 @@
-/**
- *
- *
- * @Author Marcus Caldeira
- *
- */
+* @Author Marcus Caldeira
+        *
+        */
 public class Invoice {
     //instance variables
-    String customersName;
-    Item[] items = new Item[5];
-    int numItems;
+    public String customersName;	// The variables take on whatever values you specify up here first.
+    public Item[] items = null;
+    public int numItems;
+
+    public Invoice(String customersName) {
+
+        // and then their values get changed to these.
+        this.customersName = customersName;
+        this.items = new Item[5];
+    }
+
+    public static void main(String[] args)
+    {
+        // Every instance of Invoice has totally separate customerName, items, and numItems variables.
+        Invoice myTestInvoice = new Invoice("John Smith");
+
+        Invoice anotherInvoice = new Invoice("Jane Doe");
+
+        // Will print John Smith.
+        System.out.println(myTestInvoice.customerName);
+
+        // Will print Jane Doe. They're two totally different customer names.
+        System.out.println(anotherInvoice.customerName);
+    }
 
 
 
