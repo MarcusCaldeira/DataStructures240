70138518dbe4e838e5a2f8833a9a5f3e7be7d03f
MarcusCaldeira <mac.caldr@gmail.com>
2019-09-07 18:06:10 -0500

    still adding java docs but wanted to save before computer dies

--- CaldeiraProgram1/src/Item.java
+++ CaldeiraProgram1/src/Item.java
@@ -1,6 +1,5 @@
 /**
- *
- *
+ *This class represents an item being sold in a retail store.
  * @Author Marcus Caldeira
  *August 7, 2019
  */
@@ -11,45 +10,87 @@ public class Item {
     double price;
     int currentDiscount;
 
+    /**
+     * A constructor for the Item class.
+     * @param name-The name of the item.
+     * @param weight- The weight of the item.
+     * @param price-The price of the item.
+     */
     public Item(String name, double weight, double price) {
         this.name = name;
         this.weight = weight;
         this.price = price;
         this.currentDiscount = 0;
     }
-    //getters
+
+    /**
+     * a getter for the name.
+     * @return-String name.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * A getter for the weight
+     * @return-Double weight.
+     */
     public double getWeight() {
         return weight;
     }
 
+    /**
+     * a getter for the price.
+     * @return-double price
+     */
     public double getPrice() {
         return price;
     }
 
+    /**
+     * getter for the current discount of an item.
+     * @return-int the current discount of an item.
+     */
     public int getCurrentDiscount() {
         return currentDiscount;
     }
-    //setters
+
+    /**
+     * setting the name of an item.
+     * @param name the name of an item
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * setting the weight of an item.
+     * @param weight-setting the weight of an item.
+     */
     public void setWeight(double weight) {
         this.weight = weight;
     }
 
+    /**
+     * setting the price of an item.
+     * @param price setting the price of an item.
+     */
     public void setPrice(double price) {
         this.price = price;
     }
 
+    /**
+     * setting the current discount
+     * @param currentDiscount the current discount on an item.
+     */
     public void setCurrentDiscount(int currentDiscount) {
         this.currentDiscount = currentDiscount;
     }
-    //methods
+
+    /**
+     * Takes an amount and and or subtracts the desired amount.
+     * @param amount-returns an amount.
+     */
     public void adjustPrice(double amount){
         price = price + amount;
     }
