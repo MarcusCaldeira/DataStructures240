cf2b59b0dade7f9e975c949d8556d154d5001ac9
MarcusCaldeira <mac.caldr@gmail.com>
2019-10-05 14:40:38 -0500

    ok there is one class putt together, I am curious what this will look like. I dont think I have to do a hunml diagram.

--- Program3/src/BeerNode.java
+++ Program3/src/BeerNode.java
@@ -1,2 +1,54 @@
 public class BeerNode {
+    //instance variable
+    private Beer data;
+    private BeerNode link;
+
+    //two-arg constructor
+    public BeerNode(Beer data, BeerNode link) {
+        this.data = data;
+        this.link = link;
+    }
+    //getters and setters
+    public Beer getData() {
+        return data;
+    }
+
+    public void setData(Beer data) {
+        this.data = data;
+    }
+
+    public BeerNode getLink() {
+        return link;
+    }
+
+    public void setLink(BeerNode link) {
+        this.link = link;
+    }
+    public void addBeerNodeAfter(Beer beer){
+        this.link = new BeerNode(beer, this.link);
+    }
+    public void removeBeerNodeAfter(){
+        link = link.link;
+    }
+    public static int listLength(BeerNode head){
+        BeerNode cursor;
+        int answer;
+
+
+        answer = 0;
+        for(cursor = head; cursor !=null; cursor=cursor.link)
+            answer++;
+        return answer;
+    }
+    public  static BeerNode listSearch(BeerNode head, Beer target){
+        BeerNode cursor;
+        for(cursor = head; cursor !=null; cursor = cursor.link)
+            if(target == cursor.data)
+                return cursor;
+            return null;
+    }
+
+
+
+
 }
