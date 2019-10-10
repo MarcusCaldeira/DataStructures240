fc2b0582c2cece4db6ceb99b3a4f843752df953a
MarcusCaldeira <mac.caldr@gmail.com>
2019-10-10 18:51:45 -0500

    ok doing the method to remove a beer at a specific index.

--- Program3/src/BeerLinkedBag.java
+++ Program3/src/BeerLinkedBag.java
@@ -6,7 +6,48 @@ public class BeerLinkedBag {
         head = null;
         manyItems = 0;
     };
+    public int size(){
+        return this.manyItems;
+    }
+    public void display(){
+        BeerNode cursor;
+        for(cursor = head;  cursor!=null; cursor = cursor.getLink()){
+            Beer display = cursor.getData();
+            System.out.println(display);
+        }
+    }
+    public void add(Beer beer){
+        head = new BeerNode(beer, head);
+        manyItems++;
+    }
+    public void add(int index, Beer element){
+        if(index >= 0){
+            BeerNode cursor;
+            int counter = 0;
+            for(cursor = head; cursor!=null; cursor = cursor.getLink()){
+                 counter++;
+                 if(counter == index-1 ){
+                     cursor.addBeerNodeAfter(element);
+                     return;
+                 }
+            }
+            cursor.addBeerNodeAfter(element);
+        }
+    }
+    public boolean remove(Beer target){
+        BeerNode cursor;
+        for(cursor = head; cursor!=null; cursor = cursor.getLink()){
+            if(cursor.getLink().getData().equals(target)){
+                cursor.removeBeerNodeAfter();
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean remove(Beer target){
+        BeerNode targetNode = BeerNode.listSearch(this.head,target);
 
+    }
 
 
 
--- Program3/src/BeerLinkedBagDriver.java
+++ Program3/src/BeerLinkedBagDriver.java
@@ -1,5 +1,15 @@
 public class BeerLinkedBagDriver {
     public static void main(String[] args) {
+
+
+
+        BeerLinkedBag bag1 = new BeerLinkedBag();
+        System.out.println(bag1.size());
+
+
+
+
+
         System.out.print("HI Im running");
     }
 }
