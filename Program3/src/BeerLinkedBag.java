a5e1f9b7c4314f6af679927089364d53c44bd4f6
MarcusCaldeira <mac.caldr@gmail.com>
2019-10-15 16:19:54 -0500

    alright here is the index of method. I will start to do the rest as I commit.

--- Program3/src/BeerLinkedBag.java
+++ Program3/src/BeerLinkedBag.java
@@ -2,53 +2,70 @@ import com.sun.source.tree.IfTree;
 
 public class BeerLinkedBag {
     private BeerNode head;
-    private  int manyItems;
+    private int manyItems;
 
-    public BeerLinkedBag(){
+    public BeerLinkedBag() {
         head = null;
         manyItems = 0;
-    };
-    public int size(){
+    }
+
+    ;
+
+    public int size() {
         return this.manyItems;
     }
-    public void display(){
+
+    public void display() {
         BeerNode cursor;
-        for(cursor = head;  cursor!=null; cursor = cursor.getLink()){
+        for (cursor = head; cursor != null; cursor = cursor.getLink()) {
             Beer display = cursor.getData();
             System.out.println(display);
         }
     }
-    public void add(Beer beer){
+
+    public void add(Beer beer) {
         head = new BeerNode(beer, head);
         manyItems++;
     }
-    public void add(int index, Beer element){
-        if(index >= 0){
+
+    public void add(int index, Beer element) {
+        if (index >= 0) {
             BeerNode cursor;
             int counter = 0;
-            for(cursor = head; cursor!=null; cursor = cursor.getLink()){
-                 counter++;
-                 if(counter == index-1 ){
-                     cursor.addBeerNodeAfter(element);
-                     return;
-                 }
+            for (cursor = head; cursor != null; cursor = cursor.getLink()) {
+                if (index == 0) {
+                    head = new BeerNode(element, head);
+                    manyItems++;
+                    return;
+                }
+                if (counter == index - 1) {
+                    cursor.addBeerNodeAfter(element);
+                    manyItems++;
+                    return;
+                }
+                if (counter >= manyItems) {
+                    cursor.addBeerNodeAfter(element);
+                    manyItems++;
+                }
+                counter++;
             }
-            cursor.addBeerNodeAfter(element);
         }
     }
-    public boolean remove(Beer target){
+
+    public boolean remove(Beer target) {
         BeerNode cursor;
-        for(cursor = head; cursor!=null; cursor = cursor.getLink()){
-            if(cursor.getLink().getData().equals(target)){
+        for (cursor = head; cursor != null; cursor = cursor.getLink()) {
+            if (cursor.getLink().getData().equals(target)) {
                 cursor.removeBeerNodeAfter();
                 return true;
             }
         }
         return false;
     }
-    public boolean remove(int index){
-        BeerNode targetNode = BeerNode.listPosition(this.head,index);
-        if(targetNode== null)
+
+    public boolean remove(int index) {
+        BeerNode targetNode = BeerNode.listPosition(this.head, index);
+        if (targetNode == null)
             return false;
         else {
             Beer data = this.head.getData();
@@ -58,19 +75,38 @@ public class BeerLinkedBag {
             return true;
         }
     }
-    public int countRage(Beer start){
+
+    public int countRage(Beer start) {
         int count = 0;
         BeerNode cursor = head;
-        while(cursor !=null){
+        while (cursor != null) {
             if (cursor.getData() == start)
                 count++;
-                cursor=cursor.getLink();
+            cursor = cursor.getLink();
         }
         return count;
     }
 
-
-
+    public Beer grab(int index) {
+        BeerNode cursor;
+        if (index > manyItems) {
+            return null;
+        }
+        cursor = BeerNode.listPosition(head, index);
+        return cursor.getData();
+    }
+    public int indexOf(Beer target){
+        BeerNode cursor = head;
+        int index = 1;
+        while (cursor != null){
+            if (cursor.getData().compareTo(target) == 0){
+                return index;
+            }
+            index++;
+            cursor = cursor.getLink();
+        }
+        return -1;
+    }
 
 
 }
--- Program3/src/BeerLinkedBagDriver.java
+++ Program3/src/BeerLinkedBagDriver.java
@@ -5,10 +5,13 @@ public class BeerLinkedBagDriver {
 
         BeerLinkedBag bag1 = new BeerLinkedBag();
         BeerLinkedBag bag2 = new BeerLinkedBag();
-        bag1.add(new Beer(2,"Corona","Lime"));
+        bag2.add(new Beer(2,"Corona","Lime"));
         bag2.add(0,new Beer(1,"Budwiser","Light"));
+        bag2.add(300,new Beer(1,"Budwiser","Light"));
+        bag2.add(1,new Beer(1,"Budwiser","Light"));
         bag1.display();
         System.out.println(bag1.size());
+        System.out.println(bag2.size());
 
 
 
