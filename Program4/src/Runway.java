336070c02ff55c70c0295d18fd94eb5f20f210be
MarcusCaldeira <mac.caldr@gmail.com>
2019-10-26 13:03:39 -0500

    I need to figure out how to do the startService method, and I dont exactly understand where is it.

--- Program4/src/Runway.java
+++ Program4/src/Runway.java
@@ -19,46 +19,43 @@ public class Runway{
         if (minutesForTakeoff > 0)
             minutesForTakeoff--;
     }
+    public void startService(int type){
 
-    public void startWashing()	{
-        if (minutesForTakeoff > 0)
-            throw new IllegalStateException("Washer is already busy");
-        minutesForTakeoff = minutesForLanding;
+        if(minutesForLanding == 1){
+            minutesForLanding = type;
+        }if (minutesForTakeoff == 0){
+            minutesForTakeoff = type;
+        }
+
     }
 
-	/* Exponential distribution
-	public double getNext() {
-	    return  Math.log(1-rand.nextDouble())/(-lambda);
-	}
-	*/
-
-    public static void carWashSimulate(int washTime, double arrivalProb, int totalTime)	{
+    public static void simulate(int landingTime, double arrivalProb, int totalTime)	{
         Queue<Integer> arrivalTimes = new LinkedList<Integer>();
         int next;
 
         BooleanSource arrival = new BooleanSource (arrivalProb);
-        Runway machine = new Runway(washTime);
+        Runway runway = new Runway(landingTime);
         Averager waitTimes = new Averager();
         int currentMinute;
 
-        System.out.println ("Minutes to wash one car: " + washTime);
-        System.out.print("Probability of customer arrival during a minute: ");
+        System.out.println ("Minutes to service one plane " + landingTime);
+        System.out.print("Probability of a plane arrival during a minute: ");
         System.out.println(arrivalProb);
         System.out.println("Total simulation minutes: " + totalTime);
 
-        if (washTime <= 0 || arrivalProb < 0 || arrivalProb > 1 || totalTime < 0)
+        if (landingTime <= 0 || arrivalProb < 0 || arrivalProb > 1 || totalTime < 0)
             throw new IllegalArgumentException ("Values out of range");
         for (currentMinute = 0; currentMinute < totalTime; currentMinute++)	{
             if (arrival.query())
                 arrivalTimes.add(currentMinute);
-
-            if ((!machine.isBusy()) && (!arrivalTimes.isEmpty()))	{
+            //if the runway is free and if its busy(somebody is waiting to be serviced)
+            if ((!runway.isBusy()) && (!arrivalTimes.isEmpty()))	{
                 next = arrivalTimes.remove();
                 waitTimes.addNumber(currentMinute - next);
-                machine.startWashing();
+                runway.startService(0);
             }
 
-            machine.reduceRemainingTime();
+            runway.reduceRemainingTime();
         }
 
         System.out.println("Customers served: " + waitTimes.howManyNumbers());
@@ -67,12 +64,12 @@ public class Runway{
     }
 
     public static void main(String[] args)	{
-        Runway.carWashSimulate(1, 0.7, 100000);
+        Runway.simulate(1, 0.7, 100000);
         System.out.println ("---------------------");
-        Runway.carWashSimulate(10, 0.7, 100000);
+        Runway.simulate(10, 0.7, 100000);
         System.out.println ("---------------------");
-        Runway.carWashSimulate(1, 1, 100000);
+        Runway.simulate(1, 1, 100000);
         System.out.println ("---------------------");
-        Runway.carWashSimulate(10, 1, 100000);
+        Runway.simulate(10, 1, 100000);
     }
 }
