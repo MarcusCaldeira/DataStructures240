1bb54f4194644b42faa1bec00fbb337623daec6d
MarcusCaldeira <mac.caldr@gmail.com>
2019-10-26 14:31:28 -0500

    I got the code, to print things out. but its not actually giving me real simulation data. Just the actual inputs themselves. Almost as if the logic is not working?

--- Program4/src/Runway.java
+++ Program4/src/Runway.java
@@ -20,56 +20,71 @@ public class Runway{
             minutesForTakeoff--;
     }
     public void startService(int type){
-
-        if(minutesForLanding == 1){
+        if(minutesForTakeoff > 0 || minutesForLanding > 0){
+            throw new IllegalStateException("Runway is busy");
+        }else if(type == 1){
             minutesForLanding = type;
-        }if (minutesForTakeoff == 0){
+        }if(type == 0){
             minutesForTakeoff = type;
         }
-
     }
 
-    public static void simulate(int landingTime, double arrivalProb, int totalTime)	{
+    public static void simulate(int landingTime, int takeOffTime, double landingProb, double takeoffProb, int totalTime)	{
         Queue<Integer> arrivalTimes = new LinkedList<Integer>();
+        Queue<Integer> departureTimes = new LinkedList<Integer>();
         int next;
 
-        BooleanSource arrival = new BooleanSource (arrivalProb);
+        BooleanSource arrival = new BooleanSource (landingProb);
+        BooleanSource takeOff = new BooleanSource (takeoffProb);
         Runway runway = new Runway(landingTime);
-        Averager waitTimes = new Averager();
+        Averager averageWaitTimeForLanding = new Averager();
+        Averager averageWaitTimeForTakeOff = new Averager();
         int currentMinute;
 
         System.out.println ("Minutes to service one plane " + landingTime);
         System.out.print("Probability of a plane arrival during a minute: ");
-        System.out.println(arrivalProb);
+        System.out.println(landingProb);
         System.out.println("Total simulation minutes: " + totalTime);
 
-        if (landingTime <= 0 || arrivalProb < 0 || arrivalProb > 1 || totalTime < 0)
+        if (landingTime <= 0 || landingProb < 0 || landingProb > 1 || totalTime < 0)
             throw new IllegalArgumentException ("Values out of range");
         for (currentMinute = 0; currentMinute < totalTime; currentMinute++)	{
-            if (arrival.query())
+            if (arrival.query()){
                 arrivalTimes.add(currentMinute);
+            }else if(takeOff.query()){
+                departureTimes.add(currentMinute);
+            }
+            //LANDING
             //if the runway is free and if its busy(somebody is waiting to be serviced)
-            if ((!runway.isBusy()) && (!arrivalTimes.isEmpty()))	{
+            else if ((!runway.isBusy()) && (!arrivalTimes.isEmpty()))	{
+                //we remove the landing plane and place it into next.
                 next = arrivalTimes.remove();
-                waitTimes.addNumber(currentMinute - next);
+                //As the plane is landing we remove it from the arrival(always check if there is another plane.)
+                averageWaitTimeForLanding.addNumber(currentMinute - next);
+                runway.startService(1);
+            }
+            //TAKEOFF
+            else if((!runway.isBusy()) && (!arrivalTimes.isEmpty())){
+                next = departureTimes.remove();
+                averageWaitTimeForTakeOff.addNumber(currentMinute-next);
                 runway.startService(0);
             }
-
             runway.reduceRemainingTime();
         }
 
-        System.out.println("Customers served: " + waitTimes.howManyNumbers());
-        if (waitTimes.howManyNumbers() > 0)
-            System.out.println("Average wait: " + waitTimes.average() + " sec");
+        System.out.println("Customers served: " + averageWaitTimeForLanding.howManyNumbers());
+        if (averageWaitTimeForLanding.howManyNumbers() > 0)
+            System.out.println("Average wait: " + averageWaitTimeForLanding.average() + " sec");
     }
 
     public static void main(String[] args)	{
-        Runway.simulate(1, 0.7, 100000);
+        Runway.simulate(2, 9, .50, .30,15);
         System.out.println ("---------------------");
-        Runway.simulate(10, 0.7, 100000);
-        System.out.println ("---------------------");
-        Runway.simulate(1, 1, 100000);
-        System.out.println ("---------------------");
-        Runway.simulate(10, 1, 100000);
     }
+
+
+
+
+
+
 }
