06b92a1cd48010f45a48661ab5256618a8521757
marcuscaldeira <mac.caldr@gmail.com>
2019-11-05 20:50:27 -0600

    there is the constructor for it, and we will keep on moving through the code. Wow I actually got a lot done tonight. Here are a butt ton of other methods. Im curious how much further I can get on it.

--- CaldeiraProgram5/src/Cart.java
+++ CaldeiraProgram5/src/Cart.java
@@ -1,4 +1,9 @@
 public class Cart {
     private SalesItem[] itemList;
     private int currentNumItems;
+
+    public Cart(int size) {
+        this.itemList = new SalesItem[size];
+        this.currentNumItems = 0;
+    }
 }
