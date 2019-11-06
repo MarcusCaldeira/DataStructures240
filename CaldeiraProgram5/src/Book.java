bf04650e9199ec966835949986b83dbd821e5179
marcuscaldeira <mac.caldr@gmail.com>
2019-11-05 19:56:32 -0600

    I need to figure out the monthy promo, and I know it involves logic. Lets see.

--- CaldeiraProgram5/src/Book.java
+++ CaldeiraProgram5/src/Book.java
@@ -8,8 +8,31 @@ public class Book extends SalesItem {
         this.numPages = numPages;
     }
 
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getNumPages() {
+        return numPages;
+    }
+
+    public void setNumPages(int numPages) {
+        this.numPages = numPages;
+    }
+
     @Override
     public void monthlyPromo() {
-
     }
+    @Override
+    public String toString(){
+        String str = String.format("%s\t%d\t",author,numPages);
+        return str;
+    }
+
+
+
 }
