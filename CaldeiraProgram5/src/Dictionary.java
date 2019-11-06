10c605732581f2145497a56ce964aac5ab3566cc
marcuscaldeira <mac.caldr@gmail.com>
2019-11-05 20:17:27 -0600

    ok there is another class, im really getting these out but the cart class is going to be tough, whats the deal with the polymorphism thing? I dont know yet.

--- CaldeiraProgram5/src/Dictionary.java
+++ CaldeiraProgram5/src/Dictionary.java
@@ -1,2 +1,36 @@
-public class Dictionary {
+public class Dictionary extends Book {
+    protected String language;
+    protected int numDefinition;
+
+    public Dictionary(int code, double price, int quantity, String author, int numPages, String language, int numDefinition) {
+        super(code, price, quantity, author, numPages);
+        this.language = language;
+        this.numDefinition = numDefinition;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public int getNumDefinition() {
+        return numDefinition;
+    }
+
+    public void setNumDefinition(int numDefinition) {
+        this.numDefinition = numDefinition;
+    }
+    @Override
+    public String toString(){
+        String str = String.format("%s	%s	%d	",super.toString(),language,numDefinition);
+        return str;
+    }
+    public double getRatio(){
+        double ratio = this.getNumDefinition()/super.getNumPages();
+        return ratio;
+    }
+
 }
