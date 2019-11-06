f8d906afa1bdcc2582970e4960f7bbf30502e846
marcuscaldeira <mac.caldr@gmail.com>
2019-11-05 18:30:36 -0600

    so intellij, made me include the super variables. Im curious if thats what instantiates the variables? I think there is a better way to do this.

--- CaldeiraProgram5/src/Book.java
+++ CaldeiraProgram5/src/Book.java
@@ -1,2 +1,9 @@
-public class Book {
+public class Book extends SalesItem {
+    public Book(int code, double price, int quantity) {
+        super(code, price, quantity);
+    }
+    @Override
+    public void monthlyPromo() {
+
+    }
 }
