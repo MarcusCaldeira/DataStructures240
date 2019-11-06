d2ba7639836daba8cc1f8eacb5a96568801cdcda
marcuscaldeira <mac.caldr@gmail.com>
2019-11-05 19:53:06 -0600

    here are in the start of the subclasses, Im gonna try to do all three.

--- CaldeiraProgram5/src/Book.java
+++ CaldeiraProgram5/src/Book.java
@@ -1,7 +1,13 @@
 public class Book extends SalesItem {
-    public Book(int code, double price, int quantity) {
+    protected String author;
+    protected int numPages;
+
+    public Book(int code, double price, int quantity, String author, int numPages) {
         super(code, price, quantity);
+        this.author = author;
+        this.numPages = numPages;
     }
+
     @Override
     public void monthlyPromo() {
 
