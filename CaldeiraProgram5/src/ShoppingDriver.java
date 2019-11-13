7a7a8c7b9c7a1877a7a21f042ce1e68f81b8bdf7
marcuscaldeira <mac.caldr@gmail.com>
2019-11-12 19:11:41 -0600

    there are all five items in the array. I still need to try the other method, and then to check if its actually working. I need to figure out the UML diagram.

--- CaldeiraProgram5/src/ShoppingDriver.java
+++ CaldeiraProgram5/src/ShoppingDriver.java
@@ -1,10 +1,17 @@
 public class ShoppingDriver {
     public static void main(String[] args) {
+        //Instantiate the Cart
         Cart myCart = new Cart(5);
-        Book myBook = (new Book(10,10.00,1,"MarkDavids",600));
-        myCart.addItem(myBook);
-        Dictionary myDictionary = new Dictionary(17,15.00,1,"Is",1000,"English",2000);
+        //Add A Book To The Cart.
+        myCart.addItem(new Book(10,10.00,1,"MarkDavids",600));
+        //Add A Dictionary to the Array.
+        myCart.addItem(new Dictionary(17,15.00,1,"Is",1000,"English",2000));
+        //Add A CD to the Array.
         myCart.addItem(new AudioCD(134,10.00,1,"RCA",17));
+        //Add One of two new items.
+        myCart.addItem(new Book(14,15.00,1,"MarkDavids2", 800));
+        //Add Two of two new items.
+        myCart.addItem(new AudioCD(135,10.00,1,"Columbia", 119));
 
 
         System.out.println(myCart.toString());
