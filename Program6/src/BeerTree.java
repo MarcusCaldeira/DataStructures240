b9c2f44eb6bee7bd126cfec3ca090ff52baea82c
marcuscaldeira <mac.caldr@gmail.com>
2019-11-20 16:44:41 -0600

    ok there is the depth method, i dont know if it will work though. Lets see.

--- Program6/src/BeerTree.java
+++ Program6/src/BeerTree.java
@@ -2,11 +2,37 @@ public class BeerTree {
     private BeerNode root;
 
     public void add(Beer beer){
-        if(root == null)
-            root = new BeerNode(beer, null, null);
-        else
-            add(beer);
+        root = add(root, beer);
     }
+
+    private BeerNode add(BeerNode cur, Beer beer){
+        if(cur == null)
+            return new BeerNode(beer, null, null);
+        if(beer.compareTo(cur.getData()) < 0)
+            cur.setLeftChild(add(cur.getLeftChild(), beer));
+        else if (beer.compareTo(cur.getData()) > 0)
+            cur.setRightChild(add(cur.getRightChild(), beer));
+        return cur;
+    }
+
+
+
+
+
+
+    public int depth(BeerNode beer){
+        int leftDepth = depth(beer.getLeftChild());
+        int rightDepth = depth(beer.getRightChild());
+        if (root == null)
+            return 0;
+        else
+            if(leftDepth > rightDepth)
+                return leftDepth + 1;
+            else return rightDepth +1;
+    }
+
+
+
 
 
 
