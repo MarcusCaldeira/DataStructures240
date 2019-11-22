97984c3164e3088a7cff8e23f19237a4487a5fff
marcuscaldeira <mac.caldr@gmail.com>
2019-11-21 19:09:54 -0600

    oh here are all the methods to display the list in the pre order, post order, and in order ways.

--- Program6/src/BeerTree.java
+++ Program6/src/BeerTree.java
@@ -4,7 +4,6 @@ public class BeerTree {
     public void add(Beer beer){
         root = add(root, beer);
     }
-
     private BeerNode add(BeerNode cur, Beer beer){
         if(cur == null)
             return new BeerNode(beer, null, null);
@@ -15,47 +14,53 @@ public class BeerTree {
         return cur;
     }
 
-
-
-
-
-
-    public int depth(BeerNode beer){
-        int leftDepth = depth(beer.getLeftChild());
-        int rightDepth = depth(beer.getRightChild());
-        if (root == null)
+    /**
+     * This method prints the list in PreOrder.
+     * @param node it takes in one node.
+     */
+    public void preorder(BeerNode node){
+        System.out.println(node.getData()+"\t");
+        if (node.getLeftChild() != null)
+            preorder(node.getLeftChild());
+        if(node.getRightChild() != null)
+            preorder(node.getRightChild());
+    }
+    /**
+     * This method prints in the list in Postorder
+     * @param node it takes in a node.
+     */
+    public void postorder(BeerNode node){
+        if(node.getLeftChild() != null)
+            postorder(node.getLeftChild());
+        if(node.getRightChild() != null)
+            preorder(node.getLeftChild());
+        System.out.println(node.getData()+"\t");
+    }
+    /**
+     * This method prints the list Inorder
+     * @param node
+     */
+    public void inorder(BeerNode node){
+        if(node.getLeftChild() != null)
+            inorder(node.getLeftChild());
+        System.out.println(node.getData()+"\t");
+        if(node.getRightChild() != null)
+            inorder(node.getRightChild());
+    }
+    /**
+     * This method calculates the depth of the tree
+     * @param root the starting node of three, that acts as the base to count of off.
+     * @return will return the the depth of the tree, despite which side is larger.
+     */
+    public int depth(BeerNode root){
+        int leftDepth = depth(root.getLeftChild());
+        int rightDepth = depth(root.getRightChild());
+        if (this.root == null)
             return 0;
         else
             if(leftDepth > rightDepth)
                 return leftDepth + 1;
             else return rightDepth +1;
     }
-
-
-
-
-    public int count(BeerNode root){
-        int numberOfNodes = 0;
-        if (root == null)
-            return 0;
-        if(root.getLeftChild() != null && root.getRightChild() != null)
-            numberOfNodes++;
-        numberOfNodes += (count(root.getLeftChild())+ count(root.getRightChild()));
-        return numberOfNodes;
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
